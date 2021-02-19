e9e5f3d3ca87fdedce7edb2febd03d12a36207d9
andreynorvatov <Whitesgn4@gmail.com>
2021-02-19 22:33:47 +0400

    New Static files dirs

--- store_engine/store_engine/settings.py
+++ store_engine/store_engine/settings.py
@@ -124,7 +124,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_dev'),
+    os.path.join(BASE_DIR, 'store_engine/static_dev'),
 )
 
 
